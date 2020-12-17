8e00e658b3e7ccf80bd3facd937b1c932ab940d5
Rachel Michaela <hacking@rami.org.il>
2020-12-17 13:38:49 -0600

    Support separate audio and video file formats.

--- avmerge.pl
+++ avmerge.pl
@@ -29,28 +29,41 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 use warnings;
 use strict;
 
+package avmerge;
+
 my $num_args = $#ARGV + 1;
-if ($num_args != 2) {
-	printf "Usage: avmerge import_file_extension export_file_extension\n";
+if ($num_args != 2 && $num_args != 3) {
+	printf "Usage: avmerge import_file_extension [second_import_file_extension] export_file_extension\n";
 	printf "Example: avmerge webm mkv\n";
+	printf "See man avmerge for more details.\n";
 	exit;
 }
 
-my $IMPORTEXT = $ARGV[0];
-my $EXPORTEXT = $ARGV[1];
+if ($num_args == 2) {
+	our $VIMPORTEXT = $ARGV[0];
+	our $AIMPORTEXT = $VIMPORTEXT;
+	our $EXPORTEXT = $ARGV[1];
+} else {
+	our $VIMPORTEXT = $ARGV[0];
+	our $AIMPORTEXT = $ARGV[1];
+	our $EXPORTEXT = $ARGV[2];
+}
 
 my @files = glob q{*};
 
 for (my $i = 0; $i < @files; $i++) {
-	if ($files[$i] =~ m/$IMPORTEXT/msx) {
+	if ($files[$i] =~ m/$avmerge::VIMPORTEXT/msx) {
 		printf "Found: %s\n", $files[$i];
-		if ($files[$i + 1] =~ m/$IMPORTEXT/msx) {
+		if ($files[$i + 1] =~ m/$avmerge::AIMPORTEXT/msx) {
 			printf "Matched: %s\n", $files[$i + 1];
 			my $export = $files[$i];
 			$export =~ s/[.].*//msx;
-			printf "Executing: ffmpeg -i ${files[$i]} -i ${files[$i + 1]} ${export}.$EXPORTEXT\n";
-			system "ffmpeg -i \"${files[$i]}\" -i \"${files[$i + 1]}\" \"${export}.$EXPORTEXT\"";
+			printf "Executing: ffmpeg -i ${files[$i]} -i ${files[$i + 1]} ${export}.$avmerge::EXPORTEXT\n";
+			system "ffmpeg -i \"${files[$i]}\" -i \"${files[$i + 1]}\" \"${export}.$avmerge::EXPORTEXT\"";
 			$i++;
 		}
+	} else {
+		printf("No files found.\n");
+		exit;
 	}
 }
